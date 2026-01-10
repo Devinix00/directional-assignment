8f5a14161a702310526634ad116d0b0a4bd91b41
김범수 <121074744+Devinix00@users.noreply.github.com>
2026-01-10 11:37:43 +0900

    feat: 헤더 인증/비인증 버튼 조건부 렌더링

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,11 +1,15 @@
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import { Link } from "react-router-dom";
 import PATH from "../../router/path";
 import styles from "./Header.module.scss";
+import useAuthStore from "../../stores/useAuthStore";
 
 const { Header: AntdHeader } = Layout;
 
 export default function Header() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const logout = useAuthStore((state) => state.logout);
+
   return (
     <AntdHeader className={styles.header}>
       <Link to={PATH.HOME} className={styles.logo}>
@@ -13,7 +17,17 @@ export default function Header() {
       </Link>
       <nav className={styles.nav}>
         <Link to={PATH.HOME}>홈</Link>
-        <Link to={PATH.LOGIN}>로그인</Link>
+        {isAuthenticated ? (
+          <Button
+            onClick={() => logout()}
+            type="link"
+            className={styles.logout_button}
+          >
+            로그아웃
+          </Button>
+        ) : (
+          <Link to={PATH.LOGIN}>로그인</Link>
+        )}
       </nav>
     </AntdHeader>
   );
